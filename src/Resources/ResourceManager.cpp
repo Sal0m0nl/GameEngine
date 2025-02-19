74773a34de0322140725923c531fd40c00a6a365
yebishe <raf10-10@mail.ru>
2025-02-19 17:53:12 +0300

    Added Resource Manager. It was crazy!!!!

---
+++ src/Resources/ResourceManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sal0m0n on 2/19/2025.
+//
+
+#include "ResourceManager.h"
---
+++ src/Resources/ResourceManager.h
@@ -0,0 +1,16 @@
+//
+// Created by Sal0m0n on 2/19/2025.
+//
+
+#ifndef RESOURCEMANAGER_H
+#define RESOURCEMANAGER_H
+
+
+
+class ResourceManager {
+
+};
+
+
+
+#endif //RESOURCEMANAGER_H
